785ea6d1964a24c08c462cd01ab8b008609bb94d
LeonardoPalomo <leonardo.palomo@usach.cl>
2024-03-15 22:25:30 -0300

    add logic to attend events

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -1,4 +1,36 @@
 class EventAttendancesController < ApplicationController
 
-  
+  def new
+    @event_attendance = EventAttendance.new
+  end
+
+  def show
+  end
+
+  def create
+    # p "---------------------"
+    # p @event
+    # p "---------------------"
+    @event = Event.find(params[:event_id])
+    @event_attendance = EventAttendance.new
+    @event_attendance.attended_event = @event
+    @event_attendance.event_attendee = current_user
+    # p "-------------IS THIS VALID?----------------"
+    # p @event_attendance
+    # p @event_attendance.valid?
+    # p "-------------------------------------------"
+
+    if @event_attendance.save
+      redirect_to event_url(@event), notice: "Subscription added."
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  # Only allow a list of trusted parameters through.
+  def event_attendance_params
+    params.require(:event_attendance).permit(:event_id)
+  end
+
 end
